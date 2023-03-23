cc15d4b044c7c1f99d5723660d14a3be05cacb58
Vasant_sharma <sharmavasant476@gmail.com>
2023-03-23 17:09:50 +0530

    database connected

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3300
 const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
@@ -15,7 +15,7 @@ const bodyParser = require('body-parser')
 mongoose.set('strictQuery', false);
 
 //database connection
-mongoose.connect('mongodb://localhost/health', {useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+mongoose.connect('mongodb://localhost/health', {useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('Database connected...');
