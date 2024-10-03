b1cbdb44b4415d0329e158ea24277415b17b992d
VASANT KUMAR SHARMA <sharmavasant476@gmail.com>
2024-10-03 19:14:09 +0530

    Update homeController.js

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -1,7 +1,8 @@
 function homeController() {
   return {
     index(req, res) {
-      const loggedIn = req.session.passport.user ? true : false;
+      const loggedIn =
+        req.session.passport && req.session.passport.user ? true : false;
       // console.log(req.session.passport.user);
       return res.render("home", { loggedIn: loggedIn });
     },
